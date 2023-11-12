ae9f63831ce0d78384172109e2fdbaf936e6d055
Khoi Nguyen <hi.khoi.nguyen@gmail.com>
2023-11-11 16:42:03 -0800

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="risk-of-rain-returns-save-editor/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
